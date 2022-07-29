2e6288b61b45f072a3e13616740380c37cd251d6
Nathan Alderfer <alderfer@arista.com>
2022-07-29 21:42:02 +0000

    Adding comments for clarification

--- lab3/example.py
+++ lab3/example.py
@@ -20,13 +20,15 @@ if __name__ == '__main__':
     with open('/home/coder/project/labfiles/day1/lab3/this.will.not.work.for.you') as infile:
         access_token = infile.read()
 
-    s = requests.session(verify=False)
+    s = requests.session()
     s.verify = False
     s.cookies.set("access_token", access_token)
 
+    # change imageurl to the actual endpoint you find in the REST API explorer
     r = s.get('https://{}/imageurl'.format(CVP_IP))
     response = r.json()
     
+    # change containerurl to the actual endpoint you find in the REST API explorer
     r = s.get('https://{}/containerurl'.format(CVP_IP))
     response = r.json()
 
